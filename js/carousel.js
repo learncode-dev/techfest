16bf2c646d90e673b5e7ac051f821c4c47d26d9a
LearnCode <info@learncode.es>
2024-11-27 18:06:41 +0100

    upload files

---
+++ js/carousel.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-slide");
+    let dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex - 1].classList.add("active");
+    dots[slideIndex - 1].className += " active";
+}
+
+setInterval(() => {
+    changeSlide(1);
+}, 5000);
